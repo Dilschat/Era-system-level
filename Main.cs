5536e4f488e203ff4728ecd80441ee34d802f9b3
Dilschat <dilschat@yandex.ru>
2018-10-29 12:00:40 +0300

    initial structure

---
+++ Main.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Erasystemlevel.Tokenizer;
+
+namespace HelloWorld
+{
+    class Hello
+    {
+        static void Main()
+        {
+            Tokenizer tok = new Tokenizer("/Users/dilsatsalihov/Projects/ERA-system-level/Era-system-level/Tokenizer/text.txt");
+            tok.tokenize();
+        }
+    }
+}
---
+++ Tokenizer/Tokenizer.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+
+namespace Erasystemlevel.Tokenizer
+{
+    public class Tokenizer
+    {
+        Regex numericRegex = new Regex("");
+        Regex identifierRegex = new Regex("");
+        HashSet<string> delimeters = new HashSet<string>(new List<string>() { "", "" });
+        HashSet<string> operators = new HashSet<string>(new List<string>() { "", "" });
+        HashSet<string> keyword = new HashSet<string>(new List<string>() { "", "" });
+        StreamReader reader;
+
+        public Tokenizer(string filePath)
+        {
+            FileStream fs = new FileStream(filePath, FileMode.Open,
+                FileAccess.Read);
+            this.reader = new StreamReader(fs, Encoding.UTF8);
+        }
+
+        public void tokenize()
+        {
+            while (!this.reader.EndOfStream)
+            {
+                Console.WriteLine(this.reader.ReadLine();
+            }
+        }
+
+
+
+
+
+
+    }
+}
