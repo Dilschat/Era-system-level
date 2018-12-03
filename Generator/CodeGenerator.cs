94be0adcee85af6596d0d0567739160d1456f881
Sergey <sergejrf@gmail.com>
2018-12-03 05:09:39 +0300

    Start generator

---
+++ Generator/CodeGenerator.cs
@@ -0,0 +1,16 @@
+using Erasystemlevel.Parser;
+
+namespace HelloWorld.Generator
+{
+    public class CodeGenerator
+    {
+        private AstNode root;
+        // todo: symbol table
+        
+        CodeGenerator(SemanticAnalyzer structure)
+        {
+            
+        }
+    }
+    
+}
---
+++ Generator/SemanticAnalyzer.cs
@@ -0,0 +1,7 @@
+namespace HelloWorld.Generator
+{
+    public class SemanticAnalyzer
+    {
+        
+    }
+}
