b36f9faf9abe6629d22aa0589692a5ad9d4929c9
Ecaterina Baba <e.baba@innopolis.ru>
2018-12-06 21:42:49 +0300

    assembly

--- Generator/AssemblyBuffer.cs
+++ Generator/AssemblyBuffer.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Text;
 using Erasystemlevel.Parser;
 
 namespace EraSystemLevel.Generator
@@ -13,13 +14,50 @@ namespace EraSystemLevel.Generator
 
         public void put(AstNode node)
         {
+            if (node.GetNodeType() == AstNode.NodeType.AssemblerStatement ||
+                node.GetNodeType() == AstNode.NodeType.OperationOnRegisters)
+            {
+                statements.AddLast(node);
+            }
+
             // todo: check that this node is as assembly statement and then push it to statements list
         }
 
         public override string ToString()
         {
-            // todo: generate assembly text from statements list
+            var asmCode = new StringBuilder();
+
+            foreach (var item in statements)
+            {
+                asmCode.Append(statementToString(item) + "\n");
+            }
+
             return base.ToString();
+        }
+
+        private static string statementToString(AstNode item)
+        {
+            if (item.GetNodeType() == AstNode.NodeType.OperationOnRegisters)
+            {
+                if (item.getValue().Equals(":="))
+                {
+                    // todo
+                }
+                else
+                {
+                    var childs = item.getChilds();
+                    var left = (AstNode) childs[0];
+                    var right = (AstNode) childs[1];
+                    
+                    return left.getValue() + " " + item.getValue() + " " + right.getValue() + ";";
+                }
+                
+            } else if (item.GetNodeType() == AstNode.NodeType.AssemblerStatement)
+            {
+                // todo
+            }
+
+            return "";
         }
     }
 }
